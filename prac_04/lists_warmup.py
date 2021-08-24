743f8414507ddebf902360996847e9137c7ea005
Jake <jake.edmunds@hotmail.com>
2021-08-24 13:38:24 +1000

    initial commit

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,30 @@
+"""List Warmup"""
+
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0]   A. 3
+# numbers[-1]   A. 2
+# numbers[3]    A. 1
+# numbers[:-1]  A. [3, 1, 4, 1, 5, 9]
+# numbers[3:4]  A. [1]
+# 5 in numbers  A. True
+# 7 in numbers  A. False
+# "3" in numbers    A. False
+# numbers + [6, 5, 3]   A. [3, 1, 4, 1, 5, 9, 2, 6 ,5 ,3]
+
+
+# Write Python expressions (in the same Python file) to achieve the following:
+
+numbers[0] = "10"
+
+numbers[-1] = 1
+
+print(numbers[2:])
+
+print(7 in numbers)
+
+
+
+
+
+
