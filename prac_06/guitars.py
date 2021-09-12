ec8e880ff1bc75381cd86dcd7847f0c788909b52
Jake <jake.edmunds@hotmail.com>
2021-09-12 21:53:56 +1000

    add confirmation of guitar being added to list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,16 +5,18 @@ from prac_06.guitar import Guitar
 
 def main():
     """Add guitars to a list in class format"""
-    guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
-    # guitars = []
+    # guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
+    guitars = []
     print("My guitars!")
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
         cost = int(input("Cost: $"))
         guitars.append(Guitar(name, year, cost))
+        print(guitars[-1], "added")
         name = input("Name: ")
-    print_guitars(guitars)
+    if len(guitars) > 0:
+        print_guitars(guitars)
 
 
 def print_guitars(guitars):
