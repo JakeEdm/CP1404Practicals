bc54d9267173c75ac26ada83309520482ce68a44
Jake <jake.edmunds@hotmail.com>
2021-10-18 17:36:48 +1000

    Initial commit

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,19 +1,11 @@
-"""Module docstring"""
-
 from unreliable_car import UnreliableCar
 
 
-# CONSTANTS
 
 def main():
     car = UnreliableCar("Corolla", 100, 30)
     car.drive(40)
     print(car)
-
-
-def do_stuff():
-    """Function docstring"""
-    # statements...  
 
 
 main()
